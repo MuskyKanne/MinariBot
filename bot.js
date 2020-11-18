dc6e2c970f6b6880877fcece191d6ac613e807a6
MuskyKanne <rangeltauan@gmail.com>
2020-11-18 19:14:11 -0300

    002-2

--- bot.js
+++ bot.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  const ping = new Date();
+  ping.setHours(ping.getHours() - 3);
+  console.log(`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+
 require('dotenv').config();
 const { MINA_TOKEN, DONO } = process.env;
 const { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler } = require('discord-akairo');
