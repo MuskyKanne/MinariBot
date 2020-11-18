50ae2cdc4b6caee09947c849d8a4dc977b82bc94
MuskyKanne <rangeltauan@gmail.com>
2020-11-18 19:41:33 -0300

    003

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const { MINA_TOKEN, DONO } = process.env;
 const { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler } = require('discord-akairo');
 
-const { join } = require('path');
+const path = require('path');
 
 class MyClient extends AkairoClient {
     constructor() {
@@ -22,7 +22,7 @@ class MyClient extends AkairoClient {
             disableMentions: 'everyone'
         });
         this.commandHandler = new CommandHandler(this, {
-            directory: join(__dirname, "commands"),
+            directory: path.join(__dirname, "commands"),
             prefix: 'xx!'
         });
 
