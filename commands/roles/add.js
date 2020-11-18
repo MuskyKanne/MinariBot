9cd0831ba1337c1e075328cfc6a2d2c149fede18
MuskyKanne <rangeltauan@gmail.com>
2020-11-18 20:14:46 -0300

    fix

--- commands/roles/add.js
+++ commands/roles/add.js
@@ -19,7 +19,7 @@ class PokemonCommand extends Command {
     }
 
     exec(message, args) {
-        let karina = message.guild.roles.cache.find("778586926956544020");
+        let karina = message.guild.roles.cache.find(role => role.name === '「 Karina 」');
         let giselle = message.guild.roles.cache.find("778587393128529920");
         let winter = message.guild.roles.cache.find("778587522883256341");
         let ningning = message.guild.roles.cache.find("778587286538289162");
